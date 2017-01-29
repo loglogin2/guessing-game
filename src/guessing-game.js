50e791eaff5a8326fc10fca810f5924b747745be
loglogin2 <i9991559@gmail.com>
2017-01-29 18:38:42 +0300

    g-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._min = 0;
+        this._max = 0;
+        this.current = 0;
+    }
 
     setRange(min, max) {
-
+        this._min = min;
+        this._max = max;
+        return this;
     }
 
     guess() {
-
+        this.current = Math.ceil( ( this._min + this._max ) / 2 );
+        return this.current;
     }
 
-    lower() {
 
+    lower() {
+        this._max = this.current;
+        return this._max;
     }
 
     greater() {
-
+        this._min = this.current;
+        return this._min;
     }
 }
 
